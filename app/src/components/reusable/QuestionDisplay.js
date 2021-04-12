b129d99244d067e211bdb93dc8d962e76be8b569
Kurt-Urban <80078706+Kurt-Urban@users.noreply.github.com>
2021-04-12 13:17:41 -0400

    test commit on branch

--- app/src/components/reusable/QuestionDisplay.js
+++ app/src/components/reusable/QuestionDisplay.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const QuestionDisplay = () => {};
+const QuestionDisplay = () => {
+  return (
+    <div>
+      <h1>Question Display</h1>
+    </div>
+  );
+};
 
 export default connect((state) => {})(QuestionDisplay);
